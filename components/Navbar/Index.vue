4bad52c4d29df9c8d2e86662c0fd22293dfbd6a8
Victor Barros <victorwbarros.dev@gmail.com>
2024-05-23 02:50:50 -0300

    feat: create navbar

---
+++ components/Navbar/Index.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+const actions = [
+  {
+    name:"Search",
+    href:'/search',
+    icon:'i-ic-round-search'
+  },
+  {
+    name:"List",
+    href:'/list',
+    icon:'i-ic-baseline-format-list-bulleted'
+  },
+  {
+    name:"Data Chart",
+    href:'/chart',
+    icon:'i-ic-outline-stacked-bar-chart'
+  }
+]
+</script>
+
+
+<template lang="">
+  <div class="bg-black px-4 py-2 h-16 flex justify-between items-center">
+    <div class="flex gap-4">
+
+      <template v-for="(action, index) in actions" :key="index">
+        <CollapsableButton :name="action.name" :icon="action.icon" :href = "action.href" />
+      </template>
+      
+    </div>
+    <div class="text-2xl font-bold">
+      Country <span class="text-green-600">Finder</span>
+    </div>
+    <a href="/register" class="bg-green-800 p-2 rounded-lg font-bold">Junte-se</a>
+  </div>
+</template>
+<style lang=""></style>
---
+++ components/general/CollapsableButton.vue
@@ -0,0 +1,38 @@
+<script setup>
+  import { defineProps } from "vue";
+
+  defineProps({
+    type: {
+      type: String,
+      default: "a",
+    },
+    icon: {
+      type: String,
+    },
+    iconClass: {
+      type: String,
+      default: "h-6 w-6 flex-none",
+    },
+    name: {
+      type: String,
+    },
+  });
+</script>
+
+<template lang="">
+  <component
+  :is="type"
+    href="/search"
+    class="p-1.5 rounded-full bg-neutral-200/20 hover:bg-green-700/80 hover:text-green-50 group flex justify-center items-center sm:hover:gap-1 sm:gap-0 gap-2 transition-all duration-100 delay-100 ease-in-out"
+  >
+    <UIcon :name="icon" dynamic :class="iconClass" />
+
+    <div
+      class="w-full sm:max-w-0 overflow-hidden transition-all duration-500 delay-100 ease-in-out sm:group-hover:max-w-80 truncate group-hover:px-1"
+    >
+      {{ name }}
+    </div>
+  </component>
+</template>
+
+<style lang=""></style>
