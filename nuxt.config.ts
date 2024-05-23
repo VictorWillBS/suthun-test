71ea74c2ca37535a492b4369f8d545deaf59f2eb
Victor Barros <victorwbarros.dev@gmail.com>
2024-05-23 02:49:56 -0300

    chore: configurate Nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/reset.css','~/assets/main.css'],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    },
+  ],
+  modules: ["@nuxt/ui"]
 })
