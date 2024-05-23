e279d5ee04d764721d7df7460793d6b3617a8534
Victor Barros <victorwbarros.dev@gmail.com>
2024-05-23 02:51:04 -0300

    feat: create default layout

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Navbar from "~/components/Navbar/Index.vue"
+</script>
+
+<template lang="">
+  <div class="bg-neutral-800 min-h-screen text-gray-100">
+    <Navbar/>
+    <slot/>
+  </div>
+</template>
+<style lang="">
+  
+</style>
